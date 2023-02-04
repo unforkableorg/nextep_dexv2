71aad4e40f4bca7bfb3c5fc0b7f819eebce15fe6
unforkableco <contact@unforkable.co>
2023-02-04 22:06:06 +0100

    added testnet nextep

--- src/constants/tokens/index.ts
+++ src/constants/tokens/index.ts
@@ -1,13 +1,15 @@
 import { ChainId, Token, WCXS } from '@uniswap/sdk'
 
 import MAINNET_TOKENS from './mainnet'
+import TESTNET_TOKENS from './testnet'
 
 type AllTokens = Readonly<{ [chainId in ChainId]: Readonly<{ [tokenAddress: string]: Token }> }>
 export const ALL_TOKENS: AllTokens = [
   // WCXS on all chains
   ...Object.values(WCXS),
   // chain-specific tokens
-  ...MAINNET_TOKENS
+  ...MAINNET_TOKENS,
+  ...TESTNET_TOKENS
 ]
   // remap WCXS to ETH
   .map(token => {
