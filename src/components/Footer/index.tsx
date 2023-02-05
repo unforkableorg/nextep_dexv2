37b142e3f9c980bce5b167dc17e787e31678f6a3
unforkableco <103948226+unforkableco@users.noreply.github.com>
2023-02-05 22:40:09 +0000

    remove feedback btn

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -22,11 +22,9 @@ export default function Footer() {
 
   return (
     <FooterFrame>
-      <form action="https://forms.gle/DaLuqvJsVhVaAM3J9" target="_blank">
-        <ButtonSecondary p="8px 12px">
+        {/*<ButtonSecondary p="8px 12px">
           <Send size={16} style={{ marginRight: '8px' }} /> Feedback
-        </ButtonSecondary>
-      </form>
+        </ButtonSecondary>*/}
       <ButtonSecondary onClick={toggleDarkMode} p="8px 12px" ml="0.5rem" width="min-content">
         {darkMode ? <Sun size={16} /> : <Moon size={16} />}
       </ButtonSecondary>
