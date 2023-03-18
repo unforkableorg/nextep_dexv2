e4b54abbb8f67a6fe970467cea6acebc9cd1695d
unforkableco <anton@unforkable.co>
2023-03-18 09:52:58 +0100

    Revert "fixed explorer link in notifications"
    This reverts commit 71fa6708f1255c375a6f02991769eb6eecdaeafa.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -28,7 +28,7 @@ const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
 }
 
 export function getEtherscanLink(chainId: ChainId, data: string, type: 'transaction' | 'address'): string {
-  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[785]}.nxchainscan.com`
+  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}nxchainscan.com`
 
   switch (type) {
     case 'transaction': {
